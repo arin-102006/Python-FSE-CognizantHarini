5a81a11182c8934e79ffa46a782aeb307ad50aad
Harini SG <manu.harini1234@gmail.com>
2026-07-23 15:44:56 +0530

    Added storeToRefs to ProfileView

--- student-portal-vue/src/views/ProfileView.vue
+++ student-portal-vue/src/views/ProfileView.vue
@@ -6,19 +6,25 @@
 
     <ul>
       <li
-        v-for="course in store.enrolledCourses"
+        v-for="course in enrolledCourses"
         :key="course.id"
       >
         {{ course.name }} ({{ course.credits }} Credits)
       </li>
     </ul>
 
-    <h3>Total Credits: {{ store.totalCredits }}</h3>
+    <h3>Total Credits: {{ totalCredits }}</h3>
   </div>
 </template>
 
 <script setup>
 import { useEnrollmentStore } from '../stores/enrollment'
+import { storeToRefs } from 'pinia'
 
 const store = useEnrollmentStore()
+
+const {
+  enrolledCourses,
+  totalCredits
+} = storeToRefs(store)
 </script>
